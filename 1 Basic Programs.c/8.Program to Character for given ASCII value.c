eabe7c8becda15b5ff01f97ed96bc1d0f271f0c7
AnkitaS1996 <72861391+AnkitaS1996@users.noreply.github.com>
2021-01-07 18:21:04 +0530

    Basic Programs

---
+++ 1 Basic Programs.c/8.Program to Character for given ASCII value.c
@@ -0,0 +1,27 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////   Statement =  ASCII Character To ASCII Value
+//////////////    Input  =  Single character
+/////////////    Output   =  ASCII Equivalent To given Character
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch;
+    printf("\n #######****_____ Character To ASCII Value _____****#######\n");
+
+    printf("\n======================================================\n");
+
+    printf("\n Enter Character to get its ASCII Value : ");
+    scanf("%c",&ch);
+
+    printf("\n Character of %c is = %d.",ch,ch);
+
+    printf("\n======================================================\n");
+
+    printf("\n \n Thank You...!!!!!");
+
+    printf("\n======================================================\n");
+    getch();
+    return 0;
+}
