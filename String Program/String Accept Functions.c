eaade568f9947209c6047c1bb0d6838c21d1610e
Ankita Shedage <72861391+AnkitaS1996@users.noreply.github.com>
2021-03-06 23:48:35 +0530

    String Accept Function

---
+++ String Program/String Accept Functions.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char Str[30]="";
+
+    printf("\n\t");
+
+    puts("Enter String : ");
+    printf("\n\t");
+    gets(Str);
+
+    printf("\n\t");
+
+    puts(Str);
+
+    getch();
+    return 0;
+}
