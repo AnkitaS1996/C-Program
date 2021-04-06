3151f2d3a45e79eb6e9e2481899517ef0bc80c6f
Ankita Shedage <72861391+AnkitaS1996@users.noreply.github.com>
2021-04-06 21:51:41 +0530

    Bitwise OR Operator Program

---
+++ Bitwise C Programs/02 Bitwise - OR Operator.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No1 = 0, No2 = 0,Res = 0;
+
+    printf("\n===============================================================\n");
+    printf("\n Enter Any Two Number : ");
+    scanf("%d%d",&No1,&No2);
+    printf("\n===============================================================\n");
+
+    Res = No1 | No2;
+
+    printf("\n Output Of Bitwise OR between %d & %d = %d.\n",No1,No2,Res);
+
+    printf("\n===============================================================\n");
+
+    printf("\n Thank You..\n");
+    printf("\n===============================================================\n");
+
+    getch();
+    return 0;
+}
